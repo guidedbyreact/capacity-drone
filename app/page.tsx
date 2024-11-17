a3e624b292bc4ae53dc836c83e1a296558ba8620
Olivier Vittori <olivier.vittori@gmail.comm>
2024-11-17 06:00:20 +0100

    video liens

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ function Home() {
             playsInline
             className="absolute w-full h-full object-cover"
           >
-            <source src="https://capacity-drone.s3.eu-west-3.amazonaws.com/linkedin.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIA6JKEXUS2Y6OYUM2S%2F20241117%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20241117T035932Z&X-Amz-Expires=300&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEKT%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCWV1LXdlc3QtMyJGMEQCIH2eNwznyWrr%2FQn%2BbVEU3vwXKE4%2BjFlFW2frEWxKM7ISAiBBxSIeUa4KEf1Z5tcGoMdVDPq6R2IvvThMV8%2BSrM6rsiroAgg9EAAaDDk4MjA4MTA1Mzg3NyIMoEGrTDBpkhJfo1SjKsUCVCytQo45HGKtqte2rTujAp5D6Ou%2FhZ8bxeqd1oNeotO52eO4PcRPAh1pInMbLsPZoIWqXsen6qs%2F8E8NRQhidCpgb3a%2BBdFoE1JGLVNOrMsGUOAg7c5IFflzRZ3xTPJ8zRm3abWOh9NGtH0vuHS4ADFA9epk%2FActYYs9l08e40r18GSGyF93aCz%2B1MvCiTvpqjKlr44s7tE4KzKZlawRsccM6Sl99d%2FCbizHck35A6m5r5qjERMWToPUd4YSSJsDqzzW21LUZwtTsPg9K%2BevvrpY1WoOKMkSo%2BW4sc6tJnLiyrM22V5mrrDd3UZ8LXV6myIfyh9rUOnunm7knZVbf2XYsmF1cRIrq8zMkJ3BPIy64udvExzg1F4ncKRXs6hSEHObjTEcoscobYKuLDMHnfq7UlEU6Yqhlvxv9xQtWoEzEjCPSjDE0%2BW5Bjq0AjAPhYMhnXLw92Q521koYsRaQA82Qypyyp4mmG8P8KC%2Bs6FBBH9QKD6eqhDH8Jq8ieNl9pErvN%2FetbJUe7ggIeC%2BdvxeJKjhyeeQEnI1uuvp85RzdJm4yeRcK1eaED4sP6WPAx%2BBRnrYsZzikg%2FD2baDitCCllb7BApyqFA7wSBlcN0lZSarxwJsCwK%2FJQVfG2JClknpviQiPq0r5L7jh68GkRZZ%2FVxTm61td%2BnWfQpYsj4MC42onu%2FZD2EfL9YV5JPO%2Fnqrze%2BoFHUvPpV6tXTxV%2FY3P%2F4yEH20W6PEFSuDnbT5%2BSlG0x0s9ZF7qnJOGSwYLWl%2F25656ygFBi5vtj%2BdiHPC45UHauJhBaZFK203jhlXRkfuCW7U8jGTpIcsEZPD0nwiLzlXN0V%2BldwUOy9mjBol&X-Amz-Signature=5e65622e7ebd33135e1657601b82346277a45b5d38b60df90f9943762455e358&X-Amz-SignedHeaders=host&response-content-disposition=inline" type="video/mp4" />
+            <source src="https://capacity-drone.s3.eu-west-3.amazonaws.com/linkedin.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIA6JKEXUS26SX7SRGF%2F20241117%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20241117T045900Z&X-Amz-Expires=300&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEKX%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCWV1LXdlc3QtMyJIMEYCIQD2xLPg1J9yBTrImGLlOIaWiRy1kvjL3lwe0RDtB34T0gIhAOuTqNoSet9Um6bVDpVTvflyE6O%2F4PczGVn5cG3aDQEvKugCCD4QABoMOTgyMDgxMDUzODc3IgwhLyIfGwz%2FCeUERy4qxQKsfc8pkklVSLgfXSK%2BQ2U2onATHY5pQV3XNlMWpr%2FAzZ%2BkxzxkPvLBx2j5pO%2FSUnxXRubQrhQUCSH46Of%2B76tss3ZtuLl5POP5zyhJvc8J6KwhYvZI1vd2fyOGuKQzPo4CC83VJ627ugUi%2FykfbmEB2KCrTuwTUu1uzBaxejrFt3mHwNqBz7HEW9tv8GzV7k0k%2FKsJOj3bVOgisGTEYc5ugh%2FssDc3ZmY0IqIwUVsh7tXZkUfMpEQTyoRDr86MDOA%2B%2FrLetjcVmYeXtVEzjGNlz%2BRcIHZVEISsXQ7BC8APdkAKFcpr5ZtaUGNXJrP0a0Zl44LsVFxKK5keQva6Ebo05W%2BAgR2YRsoeYVI%2FNlPkahEAG%2Flbki%2F54zkN%2BJhgsYM5wN79t%2BO7iVml2VrQCVE1Rp3lmKdMR%2BsrBFuEge7KblrpseuMMMTT5bkGOrIC8HMhXet7lkGfxT%2Fb%2B%2F2mBZ8oc3wLfKxZDQKekyPEQZmzR9eXYq94FI%2B4BNltCFwzX06HseSscUJo4xtJruvzk9Zv8ru4dh5vezRdteL0VrroR1XvCeBFoV7Kutuw%2BfoOr%2FjwGbgvIzA80BkGqPF%2FpCX0lSoxtF%2BEsO7qiOQRoz1F3E3Z2DglNdeWZG5qHPyfxRRmo8aRd8kCLffbuJhDGeCU6%2FoobGT9iuisAkeH2tkun5VHZsm0872Mtj2ifTAvUwL2gINtVjCGKJRAL3lGfSkh0jYGqNlyEpVbabFhec4Y5xN6PFLGCbZHR9YntYNIwDlESPAOvXyPatn1fLx9Y7QLHvLq6NNwQOJdVpbUKuUpmbVpPeDtrJh%2BOdprg5NbhOo4E6n8iQ59IqmTwFMUn%2BT5&X-Amz-Signature=52669b7cbef0b4775bf24a131244195f4850adcf9d37d37b047e25dfe08c6eeb&X-Amz-SignedHeaders=host&response-content-disposition=inline" type="video/mp4" />
           </video>
           {/* Overlay gradient */}
           <div className="absolute inset-0 bg-gradient-to-b from-black/50 via-black/60 to-black"></div>
@@ -27,9 +27,9 @@ function Home() {
         {/* Contenu Hero */}
         <div className="relative z-10 text-center px-4 max-w-6xl mx-auto">
           <div className="space-y-8 backdrop-blur-sm bg-black/20 p-8 rounded-2xl border border-white/10">
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 inline-block bg-gradient-to-r from-blue-600 to-blue-400 [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
-            CAPACITY DRONE
-          </h1>
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-600 to-blue-400 [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
+              CAPACITY DRONE
+            </h1>
             <p className="text-xl md:text-2xl mb-8 text-gray-300">
               L&apos;excellence des services de drones professionnels
             </p>
